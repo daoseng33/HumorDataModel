b69910a6ceeccdcc94504b5f1a8bc6a6c189c904
DAO <daoseng33@gmail.com>
2024-09-26 15:16:46 +0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "HumorDataModel",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "HumorDataModel",
+            targets: ["HumorDataModel"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/realm/realm-swift.git", .upToNextMajor(from: "10.54.0")),
+    ],
+    targets: [
+        .target(
+            name: "HumorDataModel",
+            dependencies: [
+                .product(name: "RealmSwift", package: "realm-swift")
+            ]
+        ),
+        .testTarget(
+            name: "HumorDataModelTests",
+            dependencies: ["HumorDataModel"]
+        ),
+    ]
+)
