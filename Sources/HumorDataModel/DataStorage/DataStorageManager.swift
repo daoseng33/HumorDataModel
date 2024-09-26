20f8bbdf0b6221a69fbc3252eee4da7ebebea6d7
DAO <daoseng33@gmail.com>
2024-09-26 18:01:57 +0800

    chore: add @MainActor to satisfy Swift 6

--- Sources/HumorDataModel/DataStorage/DataStorageManager.swift
+++ Sources/HumorDataModel/DataStorage/DataStorageManager.swift
@@ -8,6 +8,7 @@
 import Foundation
 import RealmSwift
 
+@MainActor
 final public class DataStorageManager {
     // MARK: - Properties
     static public let shared = DataStorageManager()
@@ -20,66 +21,64 @@ final public class DataStorageManager {
     }
     
     public func save<T: Object>(_ object: T, onError: ((Error?) -> Void)? = nil) {
-        DispatchQueue.main.async {
-            do {
-                let realm = try self.realm()
-                
-                realm.writeAsync {
-                    realm.add(object, update: .modified)
-                } onComplete: { error in
-                    onError?(error)
-                }
-                
-            } catch {
+        do {
+            let realm = try self.realm()
+            
+            realm.writeAsync {
+                realm.add(object, update: .modified)
+            } onComplete: { error in
                 onError?(error)
             }
+            
+        } catch {
+            onError?(error)
         }
     }
     
-    public func fetch<T: Object>(_ type: T.Type, completion: @escaping (Result<Array<T>, Error>) -> Void){
-        DispatchQueue.main.async {
-            do {
-                let realm = try self.realm()
-                let results = realm.objects(type)
-                let array = Array(results)
-                completion(.success(array))
-            } catch {
-                completion(.failure(error))
-            }
+    public func fetch<T: Object>(_ type: T.Type, completion: @escaping (Result<[T], Error>) -> Void) {
+        do {
+            let realm = try self.realm()
+            let results = realm.objects(type)
+            let array = Array(results)
+            completion(.success(array))
+        } catch {
+            completion(.failure(error))
         }
     }
+    
+    public func fetchAsync<T: Object>(_ type: T.Type) async throws -> [T] {
+            let realm = try self.realm()
+            let results = realm.objects(type)
+            return Array(results)
+        }
     
     public func update<T: Object>(_ object: T, with dictionary: [String: Any?], onError: ((Error?) -> Void)? = nil) {
-        DispatchQueue.main.async {
-            do {
-                let realm = try self.realm()
-                
-                realm.writeAsync {
-                    for (key, value) in dictionary {
-                        object.setValue(value, forKey: key)
-                    }
-                } onComplete: { error in
-                    onError?(error)
+        do {
+            let realm = try self.realm()
+            
+            realm.writeAsync {
+                for (key, value) in dictionary {
+                    object.setValue(value, forKey: key)
                 }
-            } catch {
+            } onComplete: { error in
                 onError?(error)
             }
+        } catch {
+            onError?(error)
         }
     }
     
     public func delete<T: Object>(_ object: T, onError: ((Error?) -> Void)? = nil) {
-        DispatchQueue.main.async {
-            do {
-                let realm = try self.realm()
-                
-                realm.writeAsync {
-                    realm.delete(object)
-                } onComplete: { error in
-                    onError?(error)
-                }
-            } catch {
+        do {
+            let realm = try self.realm()
+            
+            realm.writeAsync {
+                realm.delete(object)
+            } onComplete: { error in
                 onError?(error)
             }
+        } catch {
+            onError?(error)
         }
     }
 }
